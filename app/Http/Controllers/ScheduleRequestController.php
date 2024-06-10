6b5b774f386c1c0e02a2d9414cd348028c26ab4a
melektekm <melektekrstosm@gmail.com>
2024-06-10 21:05:50 +0300

    schedule & course filter

---
+++ app/Http/Controllers/ScheduleRequestController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ScheduleRequest;
+use Illuminate\Http\Request;
+
+class ScheduleRequestController extends Controller
+{
+    public function index()
+    {
+        return ScheduleRequest::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'course_name' => 'required|string|max:255',
+            'course_code' => 'required|string|max:255',
+            'classroom' => 'required|string|max:255',
+            'labroom' => 'required|string|max:255',
+            'classDays' => 'required|string|max:255',
+            'labDays' => 'required|string|max:255',
+            'labInstructor' => 'required|string|max:255',
+            'classInstructor' => 'required|string|max:255',
+            'scheduleType' => 'required|in:Exam,Class',
+            'status' => 'required|in:Pending,Approved',
+        ]);
+
+        $scheduleRequest = ScheduleRequest::create($request->all());
+
+        return response()->json($scheduleRequest, 201);
+    }
+    public function show($id)
+    {
+        return ScheduleRequest::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'status' => 'required|in:Pending,Approved',
+        ]);
+
+        $scheduleRequest = ScheduleRequest::findOrFail($id);
+        $scheduleRequest->update($request->all());
+
+        return response()->json($scheduleRequest, 200);
+    }
+
+    public function destroy($id)
+    {
+        ScheduleRequest::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnnouncementController;
 
 use App\Http\Controllers\ScheduleController;
-
+use App\Http\Controllers\ScheduleRequestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,20 +33,26 @@ use App\Http\Controllers\ScheduleController;
 
 
 
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/courses', [CourseController::class, 'fetchCourses']);
+    Route::put('/enroll/{id}', [CourseController::class, 'enroll']);
+});
+
 Route::post('/upload-course', [CourseController::class, 'uploadCourse']);
 Route::get('/course/name/{course_code}', [CourseController::class, 'getCourseName']);
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/schedule-requests', [ScheduleController::class, 'index']);
-    Route::post('/schedule-requests', [ScheduleController::class, 'store']);
-    Route::delete('/schedule-requests/{id}', [ScheduleController::class, 'destroy']);
+    Route::get('/schedule-requests', [ScheduleRequestController::class, 'index']);
+    Route::post('/schedule-requests', [ScheduleRequestController::class, 'store']);
+    Route::delete('/schedule-requests/{id}', [ScheduleRequestController::class, 'destroy']);
 });
 
-Route::get('/schedule-requests', [ScheduleController::class, 'index']);
-Route::post('/schedule-requests', [ScheduleController::class, 'store']);
-Route::delete('/schedule-requests/{id}', [ScheduleController::class, 'destroy']);
+// Route::get('/schedule-requests', [ScheduleController::class, 'index']);
+// Route::post('/schedule-requests', [ScheduleController::class, 'store']);
+// Route::delete('/schedule-requests/{id}', [ScheduleController::class, 'destroy']);
 
-
+Route::post('/schedule-requests', [ScheduleRequestController::class, 'store']);
 
 
 Route::middleware(['auth'])->group(function () {
